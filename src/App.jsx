ea6dc7a1129df823db3db604d3d0d1d6ae47ea08
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2023-03-08 22:42:01 +0000

    Add basic style and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from "./header"
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Header/>
     </main>
   )
 }
