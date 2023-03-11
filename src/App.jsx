9566bbfba1c81f2fe9c63adce557f115e82a0b33
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2023-03-11 14:55:11 +0000

    Add basic skeleton for the content and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Header from "./header"
+import Content from "./content"
+import Footer from "./footer"
 
 export default function App() {
   return (
     <main>
       <Header/>
+      <Content/>
+      <Footer/>
     </main>
   )
 }
