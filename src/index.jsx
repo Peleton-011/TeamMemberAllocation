f3dced9967bfeb145b392090a3f7147cbbd3ac5b
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2023-03-11 15:06:50 +0000

    Add bootstrap and slight refactor of index.jsx for readability

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const container = document.getElementById("root");
+
+const root = ReactDOM.createRoot(container)
+
+
+root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
