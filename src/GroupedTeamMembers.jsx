e84a4c6956172dcf7c2cc68896c5ae3702ced73f
Nico <nicodiazan@gmail.com>
2023-04-01 16:39:32 +0200

    Add skeleton for GroupedTeamMembers

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const GroupedTeamMembers = () => {
   return (
-    <div>GroupedTeamMembers</div>
+    <main className="container">
+        <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-B">
+                    <h1 className="mt-4 p5 rounded">Grouped Team Members</h1>
+                </div>
+            </div>
+    </main>
   )
 }
 
