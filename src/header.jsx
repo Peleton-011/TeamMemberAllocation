f91579439a07d3e1a68d31e09997212134307e2c
Nico <nicodiazan@gmail.com>
2023-04-01 11:55:23 +0200

    Fix header style

--- src/header.jsx
+++ src/header.jsx
@@ -1,8 +1,14 @@
 const Header = ({ selectedTeam, teamMemberCount }) => {
     return (
-        <header>
-            <h1 className="mt-4 p5 rounded">Team Member Allocation</h1>
-            <h3>{selectedTeam} has {teamMemberCount} members</h3>
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-B">
+                    <h1 className="mt-4 p5 rounded">Team Member Allocation</h1>
+                    <h3>
+                        {selectedTeam} has {teamMemberCount} members
+                    </h3>
+                </div>
+            </div>
         </header>
     );
 };
