ad4974fb7d09b606e4ba1e2d29988556d729fd03
Nico <nicodiazan@gmail.com>
2023-04-01 16:09:16 +0200

    Move sample employeee data to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from "./Header";
 import Employees from "./Employees";
 import Footer from "./Footer";
 
+import employeeData from "./SampleData.json";
+
 export default function App() {
     //Stateful data and such
     const [selectedTeam, setTeam] = useState(
@@ -12,92 +14,7 @@ export default function App() {
     );
 
     const [employees, setEmployees] = useState(
-        JSON.parse(localStorage.getItem("employeeList")) || [
-            {
-                id: 1,
-                fullName: "Bob Jones",
-                designation: "JavaScript Developer",
-                gender: "male",
-                teamName: "Team A",
-            },
-            {
-                id: 2,
-                fullName: "Jill Bailey",
-                designation: "Node Developer",
-                gender: "female",
-                teamName: "Team A",
-            },
-            {
-                id: 3,
-                fullName: "Gail Shepherd",
-                designation: "Java Developer",
-                gender: "female",
-                teamName: "Team A",
-            },
-            {
-                id: 4,
-                fullName: "Sam Reynolds",
-                designation: "React Developer",
-                gender: "male",
-                teamName: "Team B",
-            },
-            {
-                id: 5,
-                fullName: "David Henry",
-                designation: "DotNet Developer",
-                gender: "male",
-                teamName: "Team B",
-            },
-            {
-                id: 6,
-                fullName: "Sarah Blake",
-                designation: "SQL Server DBA",
-                gender: "female",
-                teamName: "Team B",
-            },
-            {
-                id: 7,
-                fullName: "James Bennet",
-                designation: "Angular Developer",
-                gender: "male",
-                teamName: "Team C",
-            },
-            {
-                id: 8,
-                fullName: "Jessica Faye",
-                designation: "API Developer",
-                gender: "female",
-                teamName: "Team C",
-            },
-            {
-                id: 9,
-                fullName: "Lita Stone",
-                designation: "C++ Developer",
-                gender: "female",
-                teamName: "Team C",
-            },
-            {
-                id: 10,
-                fullName: "Daniel Young",
-                designation: "Python Developer",
-                gender: "male",
-                teamName: "Team D",
-            },
-            {
-                id: 11,
-                fullName: "Adrian Jacobs",
-                designation: "Vue Developer",
-                gender: "male",
-                teamName: "Team D",
-            },
-            {
-                id: 12,
-                fullName: "Devin Monroe",
-                designation: "Graphic Designer",
-                gender: "male",
-                teamName: "Team D",
-            },
-        ]
+        JSON.parse(localStorage.getItem("employeeList")) || JSON.parse(employeeData) || []
     );
 
     useEffect(
