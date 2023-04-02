bcff117e454aa30b754bf5521bb5a79cb92dfffc
Nico <nicodiazan@gmail.com>
2023-04-02 10:13:10 +0200

    Minor tweaks to make the codebase cleaner

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useState } from "react";
 
 const GroupedTeamMembers = ({ employees, selectedTeam, setTeam, teams }) => {
-    const [groupedEmployees, setGroupData] = useState(groupTeamMembers(employees, teams));
+    const [groupedEmployees, setGroupData] = useState(
+        groupTeamMembers(employees, teams)
+    );
 
-    function groupTeamMembers(employees, teams) {GroupedTeamMembers
+    function groupTeamMembers(employees, teams) {
+        GroupedTeamMembers;
         const groupedTeams = [];
         for (let i = 0; i < teams.length; i++) {
             const team = teams[i];
@@ -25,52 +28,50 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setTeam, teams }) => {
     }
 
     function handleTeamClick(event) {
-        let newGroupData = groupedEmployees.map((groupedData) =>
-            groupedData.team === event.currentTarget.id
-                ? { ...groupedData, collapsed: !groupedData.collapsed }
-                : groupedData
+        let newTeamData = groupedEmployees.map((teamData) =>
+            teamData.team === event.currentTarget.id
+                ? { ...teamData, collapsed: !teamData.collapsed }
+                : teamData
         );
-        setGroupData(newGroupData);
+        setGroupData(newTeamData);
         setTeam(event.currentTarget.id);
     }
 
     return (
         <main className="container">
-            {groupedEmployees.map((item) => {
-                return (
-                    <div
-                        key={item.team}
-                        className="card mt-2"
-                        style={{ cursor: "pointer" }}
+            {groupedEmployees.map((thisTeam) => (
+                <div
+                    key={thisTeam.team}
+                    className="card mt-2"
+                    style={{ cursor: "pointer" }}
+                >
+                    <h4
+                        id={thisTeam.team}
+                        className="card-header text-secondary bg-white"
+                        onClick={handleTeamClick}
                     >
-                        <h4
-                            id={item.team}
-                            className="card-header text-secondary bg-white"
-                            onClick={handleTeamClick}
-                        >
-                            Team Name: {item.team}
-                        </h4>
-                        <div
-                            id={"collapse_" + item.team}
-                            className={item.collapsed ? "collapsed" : ""}
-                        >
-                            <hr />
-                            {item.members.map((member) => {
-                                return (
-                                    <div className="mt-2">
-                                        <h5 className="card-title mt-2">
-                                            <span className="text-dark">
-                                                Full name: {member.fullName}
-                                            </span>
-                                        </h5>
-                                        <p>Designation: {member.designation}</p>
-                                    </div>
-                                );
-                            })}
-                        </div>
+                        Team Name: {thisTeam.team}
+                    </h4>
+                    <div
+                        id={"collapse_" + thisTeam.team}
+                        className={thisTeam.collapsed ? "collapsed" : ""}
+                    >
+                        <hr />
+                        {thisTeam.members.map((member) => {
+                            return (
+                                <div className="mt-2" key={member.id}>
+                                    <h5 className="card-title mt-2">
+                                        <span className="text-dark">
+                                            Full name: {member.fullName}
+                                        </span>
+                                    </h5>
+                                    <p>Designation: {member.designation}</p>
+                                </div>
+                            );
+                        })}
                     </div>
-                );
-            })}
+                </div>
+            ))}
             <div className="row justify-content-center mt-3 mb-4">
                 <div className="col-B">
                     <h1 className="mt-4 p5 rounded">Grouped Team Members</h1>
